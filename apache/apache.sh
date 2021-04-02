a963a85dec6ebcd2ce79e228cba61f6b6d53691a
Alexandre CHARLES <ac@aegle.fr>
2021-04-02 11:46:50 +0200

    Release v3.0.2 + fix02 apache.sh

--- apache/apache.sh
+++ apache/apache.sh
@@ -9,15 +9,18 @@ LOCAL_CERTIFICATE=/opt/certificate
 CERTIF=certif.pem
 REP_CONF=/home/apps/apache
 REP_HTTP=/etc/httpd/conf
-REP_SSL=/etc/httpd/conf.d
+HTTPD_CONF=$REP_HTTP/httpd.conf
+SSL_CONF=/etc/httpd/conf.d/ssl.conf
 
 if test -d ${LOCAL_CERTIFICATE} -a -f ${LOCAL_CERTIFICATE}/$CERTIF; then {
 	echo "Certificat present => HTTPS"
-	sed "/<\/VirtualHost>/i# HTTPS Redirection\nRewriteEngine On\nRewriteCond %{HTTPS} off\nRewriteCond %{HTTP_HOST} !localhost\nRewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI}" $REP_HTTP/httpd.conf > /tmp/httpd.conf
-	cp -pf /tmp/httpd.conf $REP_HTTP/httpd.conf
+	sed "/<\/VirtualHost>/i# HTTPS Redirection\nRewriteEngine On\nRewriteCond %{HTTPS} off\nRewriteCond %{HTTP_HOST} !localhost\nRewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI}" $HTTPD_CONF > /tmp/httpd.conf
+	cp -pf /tmp/httpd.conf $HTTPD_CONF
 }
 else {
 	echo "Certificat non present => HTTP"
+	# Remove ssl.conf or httpd doesn't start
+	rm -f $SSL_CONF
 }
 fi
 
